f51ca82e93606bc8ea2ecb7d610040e0f18e4cf8
Marty Mulligan <whatsamarty@gmail.com>
2018-06-03 11:00:10 -0400

    remove unnecessary requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This file is for any packages you need installed for your skill to run
-sudo apt-get install python-setuptools python-dev python-pip
+#sudo apt-get install python-setuptools python-dev python-pip
