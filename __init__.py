72b334acecff8489549208d1187d78938011fde0
Marty Mulligan <whatsamarty@gmail.com>
2018-06-04 13:32:53 -0400

    compatibility updates, python3, specific on/off commands

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
-
-import re
-
-from os.path import dirname, join
-
-from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-from ouimeaux.environment import Environment
+from mycroft import intent_file_handler
+import pywemo
 
 __author__ = 'martymulligan'
 
@@ -19,104 +14,76 @@ LOGGER = getLogger(__name__)
 # "class ____Skill(MycroftSkill)"
 class WemoSkill(MycroftSkill):
 
+    devices = {}
+
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
-        super(WemoSkill, self).__init__(name="WemoSkill")
+        MycroftSkill.__init__(self)
 
-    def on_switch(self, switch):
-        LOGGER.debug("Switch detected: %s" % switch.name)
-        self.speak('Discovered a switch named ' + switch.name)
-
-    def on_motion(self, motion):
-        LOGGER.debug("Motion detected on ", motion.name)
-
-    # This method loads the files needed for the skill's functioning, and
-    # creates and registers each intent that the skill uses
     def initialize(self):
-        LOGGER.debug("Initializing WeMo Environment");
+        if len(self.devices) < 1:
+            self.discover_devices()
 
-        self.env = Environment(self.on_switch, self.on_motion)
-        self.env.start()
-        self.env.discover(seconds=5)
+    def get_device(self, name):
+        device = next((dev for dev in self.devices if dev.name == name), None)
+        if(device is None):
+            raise ValueError("I don't know a device called "+name)
+        return device
 
-        self.load_data_files(dirname(__file__))
-        prefixes = [
-            'toggle', 'tockle', 'taco']
-        self.__register_prefixed_regex(prefixes, "(?P<ToggleWords>.*)")
-
-        listprefixes = [
-            'list wemo', 'identify wemo', 'get wemo']
-        self.__register_prefixed_regex(listprefixes, "(?P<ListWords>.*)")
-
-        # switch intent
-        intent = IntentBuilder("WemoSwitchIntent").require(
-            "WemoSwitchKeyword").require("ToggleWords").build()
-        self.register_intent(intent, self.handle_wemo_switch_intent)
-
-        # discover intent
-        intent = IntentBuilder("WemoDiscoverIntent").require(
-            "WemoDiscoverKeyword").build()
-        self.register_intent(intent, self.handle_wemo_discover_intent)
-
-        # list switches intent
-        intent = IntentBuilder("WemoListIntent").require(
-            "WemoListKeyword").require("ListWords").build()
-        self.register_intent(intent, self.handle_wemo_list_intent)
-
-
-    def __register_prefixed_regex(self, prefixes, suffix_regex):
-        for prefix in prefixes:
-            self.register_regex(prefix + ' ' + suffix_regex)
-
-
-    def handle_wemo_switch_intent(self, message):
-        togglewords = message.data.get("ToggleWords")
+    @intent_file_handler('switch.on.intent')
+    def handle_switch_on(self, message):
+        if 'device' not in message.data:
+            self.speak("I don't know that device");
+            return
         try:
-            device = self.env.get_switch(togglewords)
-            device.toggle()
+            requested_device = message.data["device"];
+            device = self.get_device(requested_device)
+            device.on()
+        except ValueError as e:
+            self.speak(e)
 
-        except:
-            LOGGER.debug("Unknown WeMo device: ", togglewords)
-            self.speak("I don't know a device called ", togglewords)
-
-    def handle_wemo_list_intent(self, message):
-        # listwords are the type of thing you want to list
-        # like "mycroft list switches"
-        listwords = message.data.get("ListWords")
-        LOGGER.debug("Wemo list")
-        LOGGER.debug(listwords)
-
+    @intent_file_handler('switch.off.intent')
+    def handle_switch_off(self, message):
+        if 'device' not in message.data:
+            self.speak("I don't know that device");
+            return
         try:
-            self.env.start()
-            switches = self.env.list_switches()
-            LOGGER.debug("Wemo switches:")
-            LOGGER.debug(switches)
-            num_switches = len(switches)
+            requested_device = message.data.get("device");
+            device = self.get_device(requested_device)
+            device.off()
+        except ValueError as e:
+            self.speak(repr(e))
 
-            if num_switches > 0:
-                self.speak("I found " + str(num_switches) + " wemo switches.")
+    @intent_file_handler('toggle.intent')
+    def handle_wemo_toggle_intent(self, message):
+        requested_device = message.data.get("device")
+        try:
+            device = next((dev for dev in self.devices if dev.name == requested_device), None)
+            if device is not None:
+                device.toggle()
             else:
-                self.speak("I didn't find any wemo switches")
-
-            for switch in switches:
-                self.speak(switch)
+                self.speak("I don't know a device called ", device)
 
         except Exception as e:
-            LOGGER.debug("Error occurred listing Wemo switches: "+e.message)
-            LOGGER.debug(e)
-            self.speak("uh. ah.")
+            LOGGER.debug("WemoSkill caught an exception" + repr(e))
+            self.speak("I don't know that device")
 
-
-
-    def handle_wemo_discover_intent(self, message):
+    @intent_file_handler('list.intent')
+    def handle_wemo_list_intent(self, message):
         try:
-            self.env = Environment(self.on_switch, self.on_motion)
-            self.env.start()
-            self.env.discover(seconds=5)
+            for device in self.devices:
+                feedback = "There is a %s named %s at %s" % (device.model_name, device.name, device.host)
+                self.speak(feedback)
 
-        except:
-            LOGGER.debug("Error occurred discovering Wemo devices")
-            self.speak("ahr. ah.")
+        except Exception as e:
+            LOGGER.debug("Error occurred listing Wemo devices: " + e)
+            LOGGER.debug(e)
+            self.speak("An error occurred getting the device list")
+
+
+    @intent_file_handler('discover.intent')
+    def handle_wemo_discover_intent(self, message):
+        self.discover_devices()
 
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
@@ -124,6 +91,23 @@ class WemoSkill(MycroftSkill):
     # does nothing.
     def stop(self):
         pass
+
+    def discover_devices(self):
+        try:
+            devices = pywemo.discover_devices()
+            if len(devices) < 1:
+                self.speak("I didn't find any We Mo devices")
+            else:
+                self.devices = []
+                for device in devices:
+                    feedback = "Discovered a We Mo %s called %s" % (device.model_name, device.name)
+                    LOGGER.debug(feedback)
+                    self.devices.append(device)
+                    self.speak(feedback)
+
+        except Exception as e:
+            feedback = "Error occurred discovering Wemo switches: " + e
+            self.speak(feedback)
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
--- requirements.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#This file is for any packages you need installed for your skill to run
-#sudo apt-get install python-setuptools python-dev python-pip
